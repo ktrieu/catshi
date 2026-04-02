eef7f9f979893d4fc75a98e23127c3fbd36514f0
Kevin Trieu <kevin.trieu5813@gmail.com>
2026-04-02 01:54:42 -0400

    Setup SQLite connection

---
+++ migrations/01-user/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    discord_id TEXT NOT NULL
+);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::sync::LazyLock;
+
+use include_dir::{Dir, include_dir};
+use rusqlite::Connection;
+use rusqlite_migration::Migrations;
+
+static MIGRATIONS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/migrations");
+
+// Define migrations. These are applied atomically.
+static MIGRATIONS: LazyLock<Migrations<'static>> =
+    LazyLock::new(|| Migrations::from_directory(&MIGRATIONS_DIR).unwrap());
+
+pub fn init_db() -> anyhow::Result<Connection> {
+    let mut conn = Connection::open("./catshi.sqlite")?;
+
+    // Update the database schema, atomically
+    MIGRATIONS.to_latest(&mut conn)?;
+
+    Ok(conn)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = init_db().expect("db initialization should succeed");
+
+    conn.execute("SELECT 1 FROM users", []).unwrap();
 }
