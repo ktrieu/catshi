64fd6d3fa91fc3d5bca61c547f41826f880d15c0
Kevin Trieu <kevin.trieu5813@gmail.com>
2026-04-02 02:21:03 -0400

    Setup Serenity discord bot

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{env, sync::LazyLock};
 use include_dir::{Dir, include_dir};
 use rusqlite::Connection;
 use rusqlite_migration::Migrations;
+use serenity::{Client, all::GatewayIntents};
 
 static MIGRATIONS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/migrations");
 
@@ -19,11 +20,22 @@ pub fn init_db(path: &str) -> anyhow::Result<Connection> {
     Ok(conn)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenvy::dotenv().expect(".env loading should succeed");
 
     let db_path = env::var("DB_PATH").expect("DB_PATH should be set");
     let conn = init_db(&db_path).expect("db initialization should succeed");
+
+    let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN should be set");
+    let mut client = Client::builder(discord_token, GatewayIntents::empty())
+        .await
+        .expect("Discord client creation should succeed");
+
+    client
+        .start()
+        .await
+        .expect("Discord client start should succeed");
 
     conn.execute("SELECT 1 FROM users", []).unwrap();
 }
