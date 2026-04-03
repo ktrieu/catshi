2e4f1814a3b62fb3366385d543a81946f13bbb68
Kevin Trieu <kevin.trieu5813@gmail.com>
2026-04-03 17:50:16 -0400

    Implement user registration

---
+++ migrations/02-user-unique/up.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX users_discord_id_unique ON users(discord_id);
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::env;
 
 use serenity::{
     Client,
-    all::{Context, EventHandler, GatewayIntents, Ready},
+    all::{Context, EventHandler, GatewayIntents, GuildId, Interaction, Ready},
     async_trait,
 };
 use sqlx::SqlitePool;
 
-use crate::bot::Bot;
+mod command;
+mod store;
 
 pub async fn init_db(url: &str) -> anyhow::Result<SqlitePool> {
     let pool = SqlitePool::connect(url).await?;
@@ -17,16 +18,30 @@ pub async fn init_db(url: &str) -> anyhow::Result<SqlitePool> {
     Ok(pool)
 }
 
-struct Handler;
+struct Handler {
+    pub guild_id: GuildId,
+    pub pool: SqlitePool,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _ctx: Context, _ready: Ready) {
-        println!("connected");
+    async fn ready(&self, ctx: Context, _ready: Ready) {
+        self.guild_id
+            .create_command(ctx.http, command::register::create())
+            .await
+            .expect("command registration should succeed");
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            match command.data.name.as_str() {
+                command::register::NAME => command::register::run(&ctx, &self, &command).await,
+                _ => Ok(()),
+            }
+            .expect("command execution should succeed!");
+        };
     }
 }
-
-mod bot;
 
 #[tokio::main]
 async fn main() {
@@ -38,18 +53,18 @@ async fn main() {
         .expect("db initialization should succeed");
 
     let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN should be set");
-    let guild_id = env::var("GUILD_ID").expect("GUILD_ID should be set");
+    let guild_id = env::var("GUILD_ID")
+        .expect("GUILD_ID should be set")
+        .parse::<u64>()
+        .expect("GUILD_ID should be a valid u64");
+    let guild_id = GuildId::new(guild_id);
+
+    let handler = Handler { guild_id, pool };
 
     let mut client = Client::builder(discord_token, GatewayIntents::empty())
-        .event_handler(Handler {})
+        .event_handler(handler)
         .await
         .expect("client creation should succeed");
-
-    let bot = Bot::new(&guild_id, pool).expect("bot initialization should succeed");
-    {
-        let mut data = client.data.write().await;
-        data.insert::<Bot>(bot);
-    }
 
     client.start().await.expect("client start should succeed");
 }
