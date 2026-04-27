42aafb81e0a2a3f8d6f83f5039ad0a596f391f9c
Kevin Trieu <kevin.trieu5813@gmail.com>
2026-04-27 01:40:33 -0400

    Market details UI improvements
    Individual tables per position and also use user shortnames

--- src/command/market_details.rs
+++ src/command/market_details.rs
@@ -1,9 +1,11 @@
+use std::{cmp::Reverse, collections::HashMap};
+
 use serenity::all::{ComponentInteraction, Context};
 
 use crate::{
     Handler,
-    store::{self},
-    ui::tabulate,
+    store::{self, position::PositionWithUser},
+    ui::{self, tabulate},
     utils,
 };
 
@@ -21,41 +23,60 @@ pub async fn view_market_details(
         store::position::get_all_market_positions(&handler.pool, market_id).await?;
     all_positions.sort_by_key(|p| (p.position.instrument_id, p.position.quantity));
 
-    let mut rows = vec![[
-        "User".to_string(),
-        "Instrument".to_string(),
-        "Quantity".to_string(),
-        "Paid".to_string(),
-    ]];
+    let mut instrument_positions: HashMap<i64, Vec<&PositionWithUser>> = HashMap::new();
 
-    for p in all_positions {
+    for p in all_positions.iter() {
         if p.position.quantity == 0 {
             continue;
         }
 
+        instrument_positions
+            .entry(p.position.instrument_id)
+            .or_default()
+            .push(p);
+    }
+
+    let mut instrument_positions: Vec<(i64, Vec<&PositionWithUser>)> =
+        instrument_positions.into_iter().collect();
+
+    instrument_positions.sort_by_key(|(id, _)| *id);
+
+    let mut lines: Vec<String> = Vec::new();
+
+    for (instrument_id, positions) in instrument_positions.iter_mut() {
         let instrument_name = instruments
             .iter()
-            .find(|(i, _)| i.id == p.position.instrument_id)
-            .expect(&format!(
-                "instrument {} should exist",
-                p.position.instrument_id
-            ))
+            .find(|(i, _)| i.id == *instrument_id)
+            .expect(&format!("instrument {instrument_id} should exist",))
             .0
             .name
             .clone();
 
-        rows.push([
-            p.user.name.clone().to_string(),
-            instrument_name.clone(),
-            p.position.quantity.to_string(),
-            p.position.cost_basis.to_string(),
-        ]);
+        let mut rows = vec![[
+            "User".to_string(),
+            "Quantity".to_string(),
+            "Paid".to_string(),
+        ]];
+
+        positions.sort_by_key(|p| Reverse(p.position.quantity));
+
+        for p in positions {
+            rows.push([
+                ui::user_shortname(&p.user.name),
+                p.position.quantity.to_string(),
+                p.position.cost_basis.to_string(),
+            ]);
+        }
+        let result = tabulate(rows);
+
+        lines.push(instrument_name);
+        lines.push(result);
     }
 
-    let result = tabulate(rows);
+    let details = lines.join("\n");
 
     component
-        .create_response(&ctx.http, utils::text_interaction_response(&result, true))
+        .create_response(&ctx.http, utils::text_interaction_response(&details, true))
         .await?;
 
     Ok(())
